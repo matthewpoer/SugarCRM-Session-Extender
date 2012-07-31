68fcdf39c55b6d1da0146bb7f533480c05ef4015
Matthew Poer <mpoer@profilingsolutions.com>
2012-07-31 13:55:07 -0300

    Update PSI_Session_Length_Adjustment.php
    Minor change to see if using a String instead of an Int help set the value. I doubt it, since PHP is untyped.

--- PSI_Session_Length_Adjustment.php
+++ PSI_Session_Length_Adjustment.php
@@ -1,6 +1,6 @@
 <?php
 class PSI_Session_Length_Adjustment{
     function PSI_Session_Length_Adjustment(){
-        ini_set('session.gc_maxlifetime',28800);
+        ini_set('session.gc_maxlifetime','28800');
     }
 }
