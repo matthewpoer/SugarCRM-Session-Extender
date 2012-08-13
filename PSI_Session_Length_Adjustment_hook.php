e1922f0470eb5fa8b9d7cfe9c57db9cb9757e65a
Matthew Poer <mpoer@profilingsolutions.com>
2012-08-13 12:16:44 -0300

    Bugfix File Ref
    Noticed in some log files that this wasn't even referenced correctly, missing file extension.

--- PSI_Session_Length_Adjustment_hook.php
+++ PSI_Session_Length_Adjustment_hook.php
@@ -2,4 +2,4 @@
 $hook_version = 1;
 $hook_array = Array();
 $hook_array['after_entry_point'] = Array();
-$hook_array['after_entry_point'][] = Array(1, 'Session Length Adjustment', 'custom/PSI_Session_Length_Adjustment','PSI_Session_Length_Adjustment', 'PSI_Session_Length_Adjustment');
+$hook_array['after_entry_point'][] = Array(1, 'Session Length Adjustment', 'custom/PSI_Session_Length_Adjustment.php,'PSI_Session_Length_Adjustment', 'PSI_Session_Length_Adjustment');
